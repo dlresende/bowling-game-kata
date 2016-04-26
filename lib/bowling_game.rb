893b7813ebf907750cfdb656287855328b7862f1
Diego Lemos <diegolemosderesende@gmail.com>
2016-04-26 21:30:50 +0200

    First failing test that calculates score for simple frames.

---
+++ lib/bowling_game.rb
@@ -0,0 +1,8 @@
+
+class BowlingGame
+
+	def score(line)
+		raise RuntimeError, "not implemented"
+	end
+
+end
--- test/bowling_game_test.rb
+++ test/bowling_game_test.rb
@@ -1,8 +1,13 @@
 require 'test/unit'
+require_relative '../lib/bowling_game'
 
 class BowlingGameTest < Test::Unit::TestCase
 
-	def test_that_true_is_true
-		assert_equal true, true
+	def setup
+		@bowling = BowlingGame.new
+	end
+
+	def test_that_can_compute_score_for_simple_frames
+		assert_equal 90, @bowling.score('9-9-9-9-9-9-9-9-9-9-')
 	end
 end
