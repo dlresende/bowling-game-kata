8fdfc075141ed1aa4861b7849fa5057a3fc6a148
Diego Lemos <diegolemosderesende@gmail.com>
2016-04-27 22:28:20 +0200

    Can now compute score for simple frames.

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,8 +1,65 @@
 
 class BowlingGame
 
-	def score(line)
-		raise RuntimeError, "not implemented"
+	def initialize(parser = Parser.new)
+		@parser = parser
 	end
 
+	def score(line)
+		game = @parser.parse(line)
+		game.compute_score
+	end
+
+end
+
+class Parser
+	
+	SIMPLE_FRAME = /^(\d)(\d)/ 
+	FIRST_TRY_MISSED = /^-(\d)/
+	SECOND_TRY_MISSED = /^(\d)-/
+	FRAME_MISSED = /^--/
+
+	def parse(line)
+		case line
+		when SIMPLE_FRAME 
+			Frame.new($1, $2, parse(line[2..-1]))
+		when FIRST_TRY_MISSED
+			Frame.new(0, $1, parse(line[2..-1]))
+		when SECOND_TRY_MISSED
+			Frame.new($1, 0, parse(line[2..-1]))
+		when FRAME_MISSED
+			Frame.new(0, 0, parse(line[2..-1]))	
+		else
+			nil
+		end
+	end
+end
+
+class Frame
+
+	def initialize(first, second, nextFrame = nil)
+		@first = first.to_i
+		@second = second.to_i
+		@nextFrame = nextFrame
+	end
+
+	attr_reader :first
+	attr_reader :second
+	attr_reader :nextFrame
+
+	def compute_score
+		@first + @second + (! @nextFrame.nil? ? @nextFrame.compute_score : 0)
+	end
+
+	def to_s
+		"|#{@first},#{@second}" + (@nextFrame != nil ? @nextFrame.to_s : '|')
+	end
+
+	def ==(other)
+		other != nil &&
+			other.instance_of?(Frame) &&
+			other.first == @first	&&
+			other.second == @second	&&
+			other.nextFrame == @nextFrame
+	end
 end
--- test/bowling_game_test.rb
+++ test/bowling_game_test.rb
@@ -11,3 +11,31 @@ class BowlingGameTest < Test::Unit::TestCase
 		assert_equal 90, @bowling.score('9-9-9-9-9-9-9-9-9-9-')
 	end
 end
+
+class ParserTest < Test::Unit::TestCase
+
+	def setup
+		@parser = Parser.new
+	end
+
+	def test_that_simple_frames_can_be_parsed
+		game = Frame.new(1, 2, Frame.new(2, 3, Frame.new(3, 4)))
+
+		assert_equal game, @parser.parse('122334')
+	end
+	
+	def test_that_hyphen_is_zero
+		game = Frame.new(0, 2, Frame.new(2, 0, Frame.new(3, 4)))
+
+		assert_equal game, @parser.parse('-22-34')
+	end
+end
+
+class FrameTest < Test::Unit::TestCase
+
+	def test_that_score_is_computed_to_simple_frames
+		game = Frame.new(0, 2, Frame.new(2, 0, Frame.new(3, 4)))
+	
+		assert_equal (2 + 2 + 7), game.compute_score	
+	end
+end
