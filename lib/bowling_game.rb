45017073f6e109e5adf5bcd39621047fa6257d9c
Diego Lemos <diegolemosderesende@gmail.com>
2016-04-28 16:07:02 +0200

    Can now compute score for spares.

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -14,21 +14,36 @@ end
 
 class Parser
 	
-	SIMPLE_FRAME = /^(\d)(\d)/ 
+	SIMPLE_FRAME = /^(\d)(\d)?/ 
 	FIRST_TRY_MISSED = /^-(\d)/
 	SECOND_TRY_MISSED = /^(\d)-/
 	FRAME_MISSED = /^--/
+	SPARE_WITH_FIRST_TRY_MISSED = /^-\//
+	SPARE = /^(\d)\//
 
 	def parse(line)
+		do_parse(line, 1)
+	end
+
+	private	
+	def do_parse(line, frame_count)
 		case line
-		when SIMPLE_FRAME 
-			Frame.new($1, $2, parse(line[2..-1]))
+		when SPARE_WITH_FIRST_TRY_MISSED
+			Spare.new(0, do_parse(line[2..-1], frame_count + 1))
+		when SPARE
+			Spare.new($1, do_parse(line[2..-1], frame_count + 1))
+		when SIMPLE_FRAME
+			if frame_count > 10
+				Extra.new($1, do_parse(line[1..-1], frame_count + 1))
+			else
+				Frame.new($1, $2, do_parse(line[2..-1], frame_count + 1))
+			end
 		when FIRST_TRY_MISSED
-			Frame.new(0, $1, parse(line[2..-1]))
+			Frame.new(0, $1, do_parse(line[2..-1], frame_count + 1))
 		when SECOND_TRY_MISSED
-			Frame.new($1, 0, parse(line[2..-1]))
+			Frame.new($1, 0, do_parse(line[2..-1], frame_count + 1))
 		when FRAME_MISSED
-			Frame.new(0, 0, parse(line[2..-1]))	
+			Frame.new(0, 0, do_parse(line[2..-1], frame_count + 1))
 		else
 			nil
 		end
@@ -57,9 +72,34 @@ class Frame
 
 	def ==(other)
 		other != nil &&
-			other.instance_of?(Frame) &&
 			other.first == @first	&&
 			other.second == @second	&&
 			other.nextFrame == @nextFrame
 	end
 end
+
+class Spare < Frame
+	def initialize(first, nextFrame = nil)
+		super(first, 10 - first.to_i, nextFrame)
+	end
+
+	def compute_score
+		@first + @second + (@nextFrame.nil? ? 0 : @nextFrame.first + @nextFrame.compute_score)  	
+	end
+end
+
+class Extra < Frame
+
+	def initialize(try, nextFrame = nil)
+		super(try, 0, nextFrame)
+	end
+
+	def compute_score
+		0
+	end
+
+	def to_s
+		"|#{@first}" + (@nextFrame != nil ? @nextFrame.to_s : '|')
+	end
+end
+
--- test/bowling_game_test.rb
+++ test/bowling_game_test.rb
@@ -10,6 +10,10 @@ class BowlingGameTest < Test::Unit::TestCase
 	def test_that_can_compute_score_for_simple_frames
 		assert_equal 90, @bowling.score('9-9-9-9-9-9-9-9-9-9-')
 	end
+
+	def test_that_can_compute_score_for_spares
+		assert_equal 150, @bowling.score('5/5/5/5/5/5/5/5/5/5/5')
+	end	
 end
 
 class ParserTest < Test::Unit::TestCase
@@ -28,6 +32,38 @@ class ParserTest < Test::Unit::TestCase
 		game = Frame.new(0, 2, Frame.new(2, 0, Frame.new(3, 4)))
 
 		assert_equal game, @parser.parse('-22-34')
+	end
+
+	def test_that_slash_is_spare
+		game = Spare.new(5, Frame.new(4, 4))
+
+		assert_equal game, @parser.parse('5/44')
+	end
+
+	def test_that_extra_tries_are_handled
+		game = Spare.new(5, 
+				 Spare.new(5, 
+					   Spare.new(5, 
+						     Spare.new(5, 
+							       Spare.new(5, 
+									 Spare.new(5, 
+										   Spare.new(5, 
+											     Spare.new(5, 
+												       Spare.new(5, 
+														 Spare.new(5, 
+															   Extra.new(5)
+															  )
+														)
+												      )
+											    )
+										  )
+									)
+							      )
+						    )
+					  )
+				)
+
+		assert_equal game, @parser.parse('5/5/5/5/5/5/5/5/5/5/5')
 	end
 end
 
