c907e5ce27f1b7e1a96780c056bb7d5386476c53
Diego Lemos <diegolemosderesende@gmail.com>
2016-06-05 02:37:51 +0200

    Handling simple frames.

--- src/main/scala/BowlingGame.scala
+++ src/main/scala/BowlingGame.scala
@@ -1,5 +1,13 @@
 
 class BowlingGame {
 
-	def score(line: String):Int = throw new UnsupportedOperationException("not implemented")
+	val simpleFrame = "^(\\d)(\\d)(.*)".r
+
+	def score(line: String):Int = {
+		line.replace("-", "0") match {
+			case simpleFrame(_1, _2, next) => _1.toInt + _2.toInt + score(next)
+			case _ => 0
+		}
+	}
+
 }
--- src/test/scala/BowlingGameSuite.scala
+++ src/test/scala/BowlingGameSuite.scala
@@ -4,6 +4,11 @@ class BowlingGameSuite extends FunSuite {
 
 	test("should compute score for simple frames") {
 		val game = new BowlingGame
-		assert(game.score("9-9-9-9-9-9-9-9-9-9-") === 90)
+		assert(game.score("90909090909090909090") === 90)
+	}
+	
+	test("should replace - by 0") {
+		val game = new BowlingGame
+		assert(game.score("9--99-9-9-9-9-9-9-9-") === 90)
 	}
 }
