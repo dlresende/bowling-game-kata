da30d8d92a2acdea5d1ed527b4b09d9b36e89064
Diego Lemos <diegolemosderesende@gmail.com>
2016-06-05 03:41:04 +0200

    Handling spares.

--- src/main/scala/BowlingGame.scala
+++ src/main/scala/BowlingGame.scala
@@ -2,10 +2,16 @@
 class BowlingGame {
 
 	val simpleFrame = "^(\\d)(\\d)(.*)".r
+	val spare	= "^\\d/(\\d)(.*)".r
 
 	def score(line: String):Int = {
-		line.replace("-", "0") match {
+		computeScore(line.replace("-", "0"))
+	}
+
+	private def computeScore(line: String) = {
+		line match {
 			case simpleFrame(_1, _2, next) => _1.toInt + _2.toInt + score(next)
+			case spare(_3, next) => 10 + _3.toInt + score(_3 + next)
 			case _ => 0
 		}
 	}
--- src/test/scala/BowlingGameSuite.scala
+++ src/test/scala/BowlingGameSuite.scala
@@ -1,19 +1,22 @@
 import org.scalatest._
 
 class BowlingGameSuite extends FunSuite {
+	val game = new BowlingGame
 
 	test("should compute score for simple frames") {
-		val game = new BowlingGame
 		assert(game.score("90909090909090909090") === 90)
 	}
 	
 	test("should replace - by 0") {
-		val game = new BowlingGame
 		assert(game.score("9--99-9-9-9-9-9-9-9-") === 90)
 	}
 
 	test("should compute score for spares") {
-		val game = new BowlingGame
 		assert(game.score("5/5/5/5/5/5/5/5/5/5/5") === 150)
+		//assert(game.score("5/5/5/5/5/5/5/5/5/5/X") === 150)
+	}
+
+	test("should not compute extra frames") {
+		assert(game.score("0000000000000000005/5") === 15)
 	}
 }
